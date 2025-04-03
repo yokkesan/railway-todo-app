55f14c11c748fef64f089bfd5917fb89691234aa
ityteshima <it.yteshim@gmail.com>
2025-04-03 10:50:26 +0900

    Station03: CSS を SCSS に変換（Sass導入）完了

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,7 +6,7 @@ import { useNavigate, Redirect, Navigate } from "react-router-dom";
 import { signIn } from "../authSlice";
 import { Header } from "../components/Header";
 import { url } from "../const";
-import "./signUp.css";
+import "./signUp.scss";
 
 export const SignUp = () => {
   const navigate = useNavigate();
